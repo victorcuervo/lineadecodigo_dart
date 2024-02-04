1231f6fb93338654e10752f532790c3c257fa7da
victorcuervo <vcuervo@gmail.com>
2024-02-04 15:59:25 +0100

    Hola Mundo en Dart

--- basicos/hola-mundo.dart
+++ basicos/hola-mundo.dart
@@ -1,6 +1,10 @@
-/*
-
-
+/**
+ * @file hola-mundo.dart
+ * @version 1.0
+ * @author Víctor Cuervo - http://lineadecodigo.com
+ * @date   25/enero/2024
+ * @url  http://lineadecodigo.com/dart/xxx/
+ * @description Programa Hola Mundo en Dart
 */
 
 void main() {
